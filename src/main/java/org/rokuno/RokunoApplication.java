581c143219048187561a15b82b07d6f8ba6fbc3b
Mateus <mateusantonioofc@gmail.com>
2025-01-06 20:53:50 -0300

    feat: interface for key-value feat

--- src/main/java/org/rokuno/RokunoApplication.java
+++ src/main/java/org/rokuno/RokunoApplication.java
@@ -1,8 +1,12 @@
 package org.rokuno;
 
+import org.rokuno.core.CoreMap;
+
 public class RokunoApplication {
 
     public static void main(String[] args) {
-        
+        CoreMap db = new CoreMap();
+        db.set("kills", 100);
+        db.get("kills");
     }
 }
