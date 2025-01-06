69084103fba402f18a18f56c9ddf76bb82f5aa08
Mateus <mateusantonioofc@gmail.com>
2025-01-06 20:23:37 -0300

    feat: add exceptions errors

--- src/main/java/org/rokun/utils/InternalMap.java
+++ src/main/java/org/rokun/utils/InternalMap.java
@@ -2,6 +2,8 @@ package org.rokun.utils;
 
 import java.util.Map;
 import java.util.HashMap;
+import org.rokuno.exceptions.KeyNotFoundException;
+import org.rokuno.exceptions.NullKeyOrValueException;
 
 public class InternalMap<Key, Value> {
     
@@ -12,14 +14,23 @@ public class InternalMap<Key, Value> {
     }
     
     public void put(Key key, Value value) {
+        if (key == null || value == null) {
+            throw new NullKeyOrValueException("Key or Value connot be null");
+        }
         map.put(key, value);
     }
     
     public Value get(Key key) {
+        if (!map.containsKey(key)) {
+            throw new KeyNotFoundException("Key not found! " + key);
+        }
         return map.get(key);
     }
     
     public void remove(Key key) {
+        if (!map.containsKey(key)) {
+            throw new KeyNotFoundException("Key not found! " + key);
+        }
         map.remove(key);
     }
     
